4c316fda55f2a676e4cb58a2876ec94c3e371b26
nickysantano <nicky.santano@gmail.com>
2020-04-28 11:21:42 +0700

    update navbar
    udh berfungsi sbg fragment

--- UAS/app/src/main/java/com/example/ecurrency/MainActivity.java
+++ UAS/app/src/main/java/com/example/ecurrency/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.ecurrency;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Build;
@@ -9,12 +11,22 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.widget.Toast;
 
+import com.example.ecurrency.fragment.CalculatorFragment;
 import com.example.ecurrency.fragment.DashboardFragment;
+import com.example.ecurrency.fragment.GraphFragment;
+import com.example.ecurrency.fragment.ReminderFragment;
+import com.example.ecurrency.fragment.SettingsFragment;
 import com.luseen.spacenavigation.SpaceItem;
 import com.luseen.spacenavigation.SpaceNavigationView;
 import com.luseen.spacenavigation.SpaceOnClickListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GraphFragment GraphFragment;
+    private ReminderFragment ReminderFragment;
+    private DashboardFragment DashboardFragment;
+    private CalculatorFragment CalculatorFragment;
+    private SettingsFragment SettingsFragment;
 
     SpaceNavigationView navigationView;
 
@@ -23,28 +35,58 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        GraphFragment = new GraphFragment();
+        ReminderFragment = new ReminderFragment();
+        DashboardFragment = new DashboardFragment();
+        CalculatorFragment = new CalculatorFragment();
+        SettingsFragment = new SettingsFragment();
+
         navigationView = findViewById(R.id.main_nav);
         navigationView.initWithSaveInstanceState(savedInstanceState);
-        navigationView.addSpaceItem(new SpaceItem("", R.drawable.line_chart));
-        navigationView.addSpaceItem(new SpaceItem("", R.drawable.bell));
-        navigationView.addSpaceItem(new SpaceItem("", R.drawable.calculator));
-        navigationView.addSpaceItem(new SpaceItem("", R.drawable.ic_settings_black_24dp));
-        navigationView.changeCurrentItem(1);
+        navigationView.showIconOnly();
+
+        navigationView.addSpaceItem(new SpaceItem("nav_graph", R.drawable.line_chart));
+        navigationView.addSpaceItem(new SpaceItem("nav_reminder", R.drawable.bell));
+        navigationView.addSpaceItem(new SpaceItem("nav_calculator", R.drawable.calculator));
+        navigationView.addSpaceItem(new SpaceItem("nav_settings", R.drawable.ic_settings_black_24dp));
+//        navigationView.changeCurrentItem(DashboardFragment);
 
         navigationView.setSpaceOnClickListener(new SpaceOnClickListener() {
             @Override
             public void onCentreButtonClick() {
                 navigationView.setCentreButtonSelectable(true);
-
+                setFragment(DashboardFragment);
             }
 
             @Override
             public void onItemClick(int itemIndex, String itemName) {
+                switch(itemIndex){
+                    case 0:
+                        setFragment(GraphFragment);
+                        return;
+                    case 1:
+                        setFragment(ReminderFragment);
+                        return;
+                    case 2:
+                        setFragment(CalculatorFragment);
+                        return;
+                    case 3:
+                        setFragment(SettingsFragment);
+                        return;
+                    default:
+                        setFragment(GraphFragment);
+                        return;
+                }
             }
 
             @Override
             public void onItemReselected(int itemIndex, String itemName) {
             }
         });
+    }
+    private void setFragment (Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.container, fragment);
+        fragmentTransaction.commit();
     }
 }
