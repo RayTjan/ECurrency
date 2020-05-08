3d140ae653b9cb59fdcdfb8251c5f0977cbbda05
nickysantano <nicky.santano@gmail.com>
2020-05-08 21:25:22 +0700

    waw
    agak error

--- UAS/app/src/main/java/com/example/ecurrency/fragment/CalculatorFragment.java
+++ UAS/app/src/main/java/com/example/ecurrency/fragment/CalculatorFragment.java
@@ -48,7 +48,8 @@ public class CalculatorFragment extends Fragment implements AdapterView.OnItemSe
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calculator, container, false);
+        View view = inflater.inflate(R.layout.fragment_calculator, container, false);
+        return view;
     }
 
 
