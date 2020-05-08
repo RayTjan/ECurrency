8169469f46dcecd2e6c933dac9ccaebe7f266ebb
UnknownUncanny <50045064+UnknownUncanny@users.noreply.github.com>
2020-05-08 08:45:40 +0700

    2ndGraph

--- UAS/app/src/main/java/com/example/ecurrency/fragment/DashboardFragment.java
+++ UAS/app/src/main/java/com/example/ecurrency/fragment/DashboardFragment.java
@@ -1,6 +1,7 @@
 package com.example.ecurrency.fragment;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -16,7 +17,13 @@ import android.view.ViewGroup;
 
 import com.example.ecurrency.R;
 import com.example.ecurrency.adapter.CardNews;
+import com.example.ecurrency.adapter.idrArray;
 import com.example.ecurrency.model.News;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 
@@ -43,14 +50,47 @@ public class DashboardFragment extends Fragment {
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_dashboard, container, false);
     }
-
+    LineChart lineChart;
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
         super.onViewCreated(view, savedInstanceState);
         rvNews = view.findViewById(R.id.rv_news);
+        lineChart = view.findViewById(R.id.dash_graph);
+        ChartData();
         getNews();
     }
+    private void ChartData(){
+        ArrayList<Entry> dataSet = new ArrayList<>();
+        for (int a=0;a<=5;a++){
+            float fill =  idrArray.IDRarray.get(a).floatValue();
+            dataSet.add(new Entry(a,fill ));
+        }
+        showChart(dataSet);
+    }
 
+    public void showChart(ArrayList<Entry> data){
+        LineDataSet lineDataSet = new LineDataSet(data  ,"IDR");
+//        LineDataSet lineDataSet2 = new LineDataSet(ChartData2(),"data2 set");
+        ArrayList<ILineDataSet> iLineDataSets = new ArrayList<>();
+        iLineDataSets.add(lineDataSet);
+//        iLineDataSets.add(lineDataSet2);
+
+        LineData lineData = new LineData(iLineDataSets);
+        lineChart.setData(lineData);
+        lineChart.invalidate();
+//        lineDataSet.enableDashedLine(10f, 5f, 0f);
+        lineDataSet.enableDashedHighlightLine(10f, 5f, 0f);
+        lineDataSet.setColor(Color.DKGRAY);
+        lineDataSet.setCircleColor(Color.DKGRAY);
+        lineDataSet.setLineWidth(1f);
+        lineDataSet.setCircleRadius(3f);
+        lineDataSet.setDrawCircleHole(false);
+        lineDataSet.setValueTextSize(9f);
+        lineDataSet.setDrawFilled(true);
+        lineDataSet.setFormLineWidth(1f);
+        //background color
+        lineChart.setBackgroundColor(Color.WHITE);
+    }
     public void getNews(){
         final ArrayList<News> News = new ArrayList<>();
         AsyncHttpClient client = new AsyncHttpClient();
